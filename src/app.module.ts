ce02aed477edd2e6a779d2f3ad671298b660912e
Aditya Shahare <shahare.aditya@gmail.com>
2024-03-24 00:22:57 +0530

    fix: config and mongo atlas integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { BudgetsModule } from './budgets/budgets.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     BudgetsModule,
     UsersModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/', {
-      dbName: 'budget-app',
+    ConfigModule.forRoot({
+      isGlobal: true, // This makes the ConfigModule global, so you don't need to import it in other modules
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'), // Assuming you have MONGODB_URI in your .env file
+        dbName: 'budget-app',
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
